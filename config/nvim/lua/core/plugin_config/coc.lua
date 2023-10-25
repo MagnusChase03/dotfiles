9cf0c5e2b68ba8cea11b303915e4f7a0ef46856f
Zachary <zachness2003@gmail.com>
2023-10-24 19:26:24 -0500

    Rust

--- config/nvim/lua/core/plugin_config/coc.lua
+++ config/nvim/lua/core/plugin_config/coc.lua
@@ -5,4 +5,6 @@ function _G.check_back_space()
     return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
 end
 
+local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
 vim.keymap.set("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+vim.keymap.set("i", "<c-space>", [[coc#pum#visible() ? coc#pum#confirm() : ""]], opts)
